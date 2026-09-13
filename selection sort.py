475c95aeb54edd336c068985398860fa9bbd2fcd
Andrey_Makarov <64976956+AndreyMakarov-lol@users.noreply.github.com>
2026-09-13 15:35:15 +0300

    Добавил сортировку выбором

--- selection sort.py
+++ selection sort.py
@@ -1,2 +1,21 @@
 '''Сортировка выбором из книги "Грокаем алгоритмы второе издание"'''
+#Функция для поиска наименьшего элемента массива
+def findSmallest (arr):
+    smallest = arr[0]    #Для хранения наименьшего значения
+    smallest_index = 0   #Для хранения индекса наименьшего значения
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
 
+#Функция сортировки выбором
+def selectionSort(arr):  #Сортируем массив
+    newArr = []
+    copiedArr = list(arr) #Копируем сортируемый массив перед изменением
+    for i in range(len(copiedArr)):
+        smallest = findSmallest(copiedArr) #Находим наименьший элемент
+        newArr.append(copiedArr.pop(smallest)) #Добавляем его в новый массив
+    return newArr
+
+print(selectionSort([99,1,6,8,45,12,11]))
